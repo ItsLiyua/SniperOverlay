12da413c01311e9f85b226acc0338af1ef18ae0f
Trqhxrd <trqhxrd@duck.com>
2023-05-27 11:47:32 +0200

    Jar is now executable.

---
+++ src/main/kotlin/me/liyua/sniperoverlay/log/listeners/LobbyListener.kt
@@ -0,0 +1,13 @@
+package me.liyua.sniperoverlay.log.listeners
+
+import me.liyua.sniperoverlay.log.LogListener
+
+object LobbyListener:LogListener {
+    override fun accept(message: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun apply(message: String) {
+        TODO("Not yet implemented")
+    }
+}
